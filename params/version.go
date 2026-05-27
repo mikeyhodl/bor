d02f290c828fdd156f70f52de057d0d419ebd241
Lucca Martins <lucca_martins30@yahoo.com.br>
2026-05-27 16:32:02 -0300

    params: version bump to v2.8.3-beta2

--- params/version.go
+++ params/version.go
@@ -26,7 +26,7 @@ const (
 	VersionMajor = 2  // Major version component of the current release
 	VersionMinor = 8  // Minor version component of the current release
 	VersionPatch = 3  // Patch version component of the current release
-	VersionMeta  = "beta" // Version metadata to append to the version string
+	VersionMeta  = "beta2" // Version metadata to append to the version string
 )
 
 var (
